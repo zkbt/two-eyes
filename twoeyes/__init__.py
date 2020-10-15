342dac8c44f69926cae50d3b7fa0421ea3c5631b
Zach Berta-Thompson <zach.bertathompson@colorado.edu>
2020-10-15 08:50:38 -0600

    remove other files

--- twoeyes/__init__.py
+++
@@ -1,2 +0,0 @@
-from .stereo import *
-from .interface import *
--- twoeyes/imports.py
+++
@@ -1,9 +0,0 @@
-import os, sys, warnings
-import matplotlib.pyplot as plt, numpy as np
-
-warnings.filterwarnings("ignore")
-from PIL import Image
-
-
-import pkg_resources
-data_directory = pkg_resources.resource_filename('twoeyes', 'data')
